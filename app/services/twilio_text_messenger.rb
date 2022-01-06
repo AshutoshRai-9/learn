6d8de8a36d9f78ffe9241b94ce4b109a296412d0
Ashutosh <ashutoshr@chetu.com>
2022-01-07 00:40:13 +0530

    fix issue

--- app/services/twilio_text_messenger.rb
+++ app/services/twilio_text_messenger.rb
@@ -7,11 +7,9 @@ class TwilioTextMessenger
 
   def call
   	account_sid= "ACf2a0ddc9525639eb50fa687dc03e19b1"
-  	account_auth= "9bfa960237a0ea5040f897dc7554b71b"
+  	account_auth= "0b01d997e770edb93d689c22c8ab673c"
     client = Twilio::REST::Client.new(account_sid,account_auth)
-    client.messages.create({from: "+14153601772",
-    	to: '+919793490339',
-    	body: message})
+    client.messages.create({from:"+14153601772",to:'+919793490339',body:message})
 
      
   end
